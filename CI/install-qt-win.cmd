7e94d8ce46ae0ae835d130b3c34b1be3700d4d64
Yibai Zhang <xm1994@gmail.com>
2020-12-19 18:45:49 +0800

    chore(CI): Migrate QT to 5.15.2 for windows.
    Signed-off-by: Yibai Zhang <xm1994@gmail.com>

--- CI/install-qt-win.cmd
+++ CI/install-qt-win.cmd
@@ -1,6 +1,6 @@
 if not exist %QtBaseDir% (
-	curl -kLO https://cdn-fastly.obsproject.com/downloads/Qt_5.10.1.7z -f --retry 5 -z Qt_5.10.1.7z
-    7z x Qt_5.10.1.7z -o%QtBaseDir%
+	curl -kLO https://github.com/summershrimp/obs-auto-subtitle/releases/download/0.2.2/QT_5.15.2.7z -f --retry 5 -z QT_5.15.2.7z
+    7z x QT_5.15.2.7z -o%QtBaseDir%
 ) else (
 	echo "Qt is already installed. Download skipped."
 )
