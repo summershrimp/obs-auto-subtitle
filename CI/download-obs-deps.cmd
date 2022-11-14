7c7dc0258c3aa399e34e290f140dffab0c051643
Yibai Zhang <xm1994@gmail.com>
2022-11-14 20:14:21 +0800

    feat(build): Change build system to obs-plugintemplate
    Use obs-plugintemplate to feat obs build for all platforms.
    
    Signed-off-by: Yibai Zhang <xm1994@gmail.com>

--- CI/download-obs-deps.cmd
+++
@@ -1,6 +0,0 @@
-if not exist %DepsBasePath% (
-    curl -o %DepsBasePath%.zip -kLO https://cdn-fastly.obsproject.com/downloads/dependencies2019.zip -f --retry 5 -C -
-    7z x %DepsBasePath%.zip -o%DepsBasePath%
-) else (
-    echo "OBS dependencies are already there. Download skipped."
-)
--- CI/install-qt-win.cmd
+++
@@ -1,8 +0,0 @@
-if not exist %QtBaseDir% (
-	curl -kLO https://github.com/summershrimp/obs-auto-subtitle/releases/download/0.2.2/QT_5.15.2.7z -f --retry 5 -z QT_5.15.2.7z
-    7z x QT_5.15.2.7z -o%QtBaseDir%
-) else (
-	echo "Qt is already installed. Download skipped."
-)
-
-dir %QtBaseDir%
