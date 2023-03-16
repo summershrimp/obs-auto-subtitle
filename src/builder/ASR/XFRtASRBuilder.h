9703dff3fbd0f11e6ead7a22e4c2b92d9b8b91e9
Estela ad Astra <i@estela.cn>
2023-03-17 01:29:20 +0800

    Bugfix: fix build on case sensitive systems

--- src/builder/ASR/XFRtASRBuilder.h
+++ src/builder/ASR/XFRtASRBuilder.h
@@ -22,7 +22,7 @@ along with this program; If not, see <https://www.gnu.org/licenses/>
 #include <QString>
 
 #include "../base/ASRBuilderBase.h"
-#include "../../vendor/ASR/XfRtASR.h"
+#include "../../vendor/ASR/XFRtASR.h"
 
 class XFRtASRBuilder : public ASRBuilderBase {
 public:
